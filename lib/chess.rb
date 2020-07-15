35feb4246722cd947b35c1863f262f2be94689f0
Zac <zjcrumpton@gmail.com>
2020-07-15 15:50:23 -0600

    define Chess class with initialize function to create board

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+# represents the entire game of Chess itself
+class Chess
+  attr_accessor :board
+  def initialize
+    @board = Array.new(8) {Array.new(8)}
+  end
+
+end
