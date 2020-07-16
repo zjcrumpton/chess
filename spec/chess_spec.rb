62eb8a34fd32fb06f371f96f42e50b455b72f67e
Zac <zjcrumpton@gmail.com>
2020-07-16 10:30:08 -0600

    refactor tests

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -4,22 +4,14 @@ describe Chess do
   describe "#initialize" do
     it "creates an 8x8 board" do
       game = Chess.new
-      expect(game.board.length).to eql(8)
-      expect(game.board[0].length).to eql(8)
-      expect(game.board[1].length).to eql(8)
-      expect(game.board[2].length).to eql(8)
-      expect(game.board[3].length).to eql(8)
-      expect(game.board[4].length).to eql(8)
-      expect(game.board[5].length).to eql(8)
-      expect(game.board[6].length).to eql(8)
-      expect(game.board[7].length).to eql(8)
-      expect(game.board[8].nil?).to eql(true)
+
+      expect(game.board.all? {|row| row.length == 8}).to eql(true)
     end
 
     it "fills the board with Squares" do
       game = Chess.new
 
-      expect(game.board.each {|row| row.all? {|i| i.class == Square}})
+      expect(game.board.all? {|row| row.all? {|i| i.class == Square}}).to eql(true)
     end
 
     it "fills the pawn rows" do 
