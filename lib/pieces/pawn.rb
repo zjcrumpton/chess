c4e87a3c144b41e3704a9367a4a2847959230f27
Zac <zjcrumpton@gmail.com>
2020-07-16 11:14:46 -0600

    add pieces folder with Pawn class

---
+++ lib/pieces/pawn.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require './lib/piece_factory.rb'
+
+# represents the pawn piece in chess
+class Pawn < PieceFactory
+  def find_moves
+  end
+end
--- lib/square.rb
+++ lib/square.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require './lib/piece_factory.rb'
+require 'require_all'
+require_all './lib/pieces'
 
 class Square
   attr_accessor :piece
