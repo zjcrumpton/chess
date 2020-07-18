d0d5d61be9b2697627ca1e353234c4b042f390d0
Zac <zjcrumpton@gmail.com>
2020-07-17 20:05:19 -0600

    add valid move spec and refactor movement

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -4,14 +4,20 @@ describe Pawn do
   describe "move" do
     it "moves forward one space" do
       game = Chess.new
-      game.board.squares[6][0].piece.move('a3')
-      expect(game.board.squares[5][0].piece.class).to eql(Pawn)
+      game.piece_at('a2').move_to('a3')
+      expect(game.piece_at('a3').class).to eql(Pawn)
     end
 
     it "removes pawn from original space" do
       game = Chess.new
-      game.board.squares[6][0].piece.move('a3')
-      expect(game.board.squares[6][0].piece.nil?).to eql(true)
+      game.piece_at('a2').move_to('a3')
+      expect(game.piece_at('a2').nil?).to eql(true)
+    end
+
+    it "can only make valid moves" do
+      game = Chess.new
+      game.piece_at('a2').move_to('b3')
+      expect(game.piece_at('b3')).to eql(nil)
     end
   end
 
