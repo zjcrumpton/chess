d1c0e70fbbf191a329bee54891567dc85a507a56
Zac <zjcrumpton@gmail.com>
2020-07-25 10:23:56 -0600

    add gaurd clauses to RookMoves

--- lib/modules/movement/rook_moves.rb
+++ lib/modules/movement/rook_moves.rb
@@ -8,18 +8,22 @@ module RookMoves
       return @move = nil if @board.squares[@square.row - @i].nil?
 
       @move = @board.squares[@square.row - @i][@square.column]
+      @move = nil if @move.row > @square.row
     elsif direction == 'left'
       return @move = nil if @board.squares[@square.row][@square.column - @i].nil?
 
       @move = @board.squares[@square.row][@square.column - @i]
+      @move = nil if @move.column > @square.column
     elsif direction == 'right'
       return @move = nil if @board.squares[@square.row][@square.column + @i].nil?
 
       @move = @board.squares[@square.row][@square.column + @i]
+      @move = nil if @move.column < @square.column
     elsif direction == 'down'
       return @move = nil if @board.squares[@square.row + @i].nil?
 
       @move = @board.squares[@square.row + @i][@square.column]
+      @move = nil if @move.row < @square.row
     end
   end
 end
