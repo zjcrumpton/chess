479a6123390d408fe04cd920e4f6a37b725d69fb
Zac <zjcrumpton@gmail.com>
2020-07-25 10:25:24 -0600

    add gaurd clauses to BishopMoves

--- lib/modules/movement/bishop_moves.rb
+++ lib/modules/movement/bishop_moves.rb
@@ -9,21 +9,33 @@ module BishopMoves
       return @move = nil if @board.squares[@square.row - @i][@square.column - @i].nil?
 
       @move = @board.squares[@square.row - @i][@square.column - @i]
+      return @move = nil if @move.column > @square.column
+
+      @move = nil if @move.row > @square.row
     elsif direction == 'dl_diag'
       return @move = nil if @board.squares[@square.row + @i].nil?
       return @move = nil if @board.squares[@square.row + @i][@square.column - @i].nil?
 
       @move = @board.squares[@square.row + @i][@square.column - @i]
+      return @move = nil if @move.column > @square.column
+
+      @move = nil if @move.row < @square.row
     elsif direction == 'ur_diag'
       return @move = nil if @board.squares[@square.row - @i].nil?
       return @move = nil if @board.squares[@square.row - @i][@square.column - @i].nil?
 
       @move = @board.squares[@square.row - @i][@square.column + @i]
+      return @move = nil if @move.row > @square.row
+
+      @move = nil if @move.column < @square.column
     elsif direction == 'dr_diag'
       return @move = nil if @board.squares[@square.row + @i].nil?
       return @move = nil if @board.squares[@square.row + @i][@square.column + @i].nil?
 
       @move = @board.squares[@square.row + @i][@square.column + @i]
+      return @move = nil if @move.column < @square.column
+
+      @move = nil if @move.row < @square.row
     end
   end
 end
