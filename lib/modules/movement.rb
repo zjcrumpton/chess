b85a8a86bff27819f9e3129ddf0d517a9ab762ce
Zac <zjcrumpton@gmail.com>
2020-07-27 11:03:34 -0600

    bugfix for ur_diag movement

--- lib/modules/movement.rb
+++ lib/modules/movement.rb
@@ -22,7 +22,7 @@ module MoveList
       @move = nil if @move.row < @square.row
     elsif direction == 'ur_diag'
       return @move = nil if @board.squares[@square.row - @i].nil?
-      return @move = nil if @board.squares[@square.row - @i][@square.column - @i].nil?
+      return @move = nil if @board.squares[@square.row - @i][@square.column + @i].nil?
 
       @move = @board.squares[@square.row - @i][@square.column + @i]
       return @move = nil if @move.row > @square.row
